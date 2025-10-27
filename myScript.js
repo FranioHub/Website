f69ee16e01a98d2c0f727f7188101466abdd1f0f
FranioHub <franciszek4508@gmail.com>
2025-10-27 13:30:39 +0100

    AHH FØRSTE COMMIT

---
+++ myScript.js
@@ -0,0 +1,19 @@
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+showPanel(0,'#a4f52a');
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="green";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+
+
+}
