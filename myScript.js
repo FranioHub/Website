80481b2f6e4bb622761225423ef4d588a49ad385
FranioHub <franciszek4508@gmail.com>
2025-11-13 09:30:50 +0100

    sæt ny billeder, denne version har bare billeder uden zoom

--- myScript.js
+++ myScript.js
@@ -3,17 +3,34 @@ var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
 showPanel(0,'#a4f52a');
 
 function showPanel(panelIndex,colorCode) {
-    tabButtons.forEach(function(node){
-        node.style.backgroundColor="";
-        node.style.color="";
+    tabButtons.forEach(function (node) {
+        node.style.backgroundColor = "";
+        node.style.color = "";
     });
-    tabButtons[panelIndex].style.backgroundColor=colorCode;
-    tabButtons[panelIndex].style.color="green";
-    tabPanels.forEach(function(node){
-        node.style.display="none";
+    tabButtons[panelIndex].style.backgroundColor = colorCode;
+    tabButtons[panelIndex].style.color = "green";
+    tabPanels.forEach(function (node) {
+        node.style.display = "none";
     });
-    tabPanels[panelIndex].style.display="block";
-    tabPanels[panelIndex].style.backgroundColor=colorCode;
-
-
+    tabPanels[panelIndex].style.display = "block";
+    tabPanels[panelIndex].style.backgroundColor = colorCode;
 }
+// chat kode
+    const imageList = [
+        "TortoiseImages/pic1.jpg",
+        "https://picsum.photos/400/300?random=4",
+        "TortoiseImages/pic2.jpg",
+        "https://picsum.photos/300/300?random=3",
+        "https://picsum.photos/200/200?random=5"
+    ];
+
+    const container = document.getElementById("imageContainer"); /*billed spawn point*/
+
+// Add each image to the container
+    imageList.forEach(url => {
+        const img = document.createElement("img");
+        img.src = url;
+        img.alt = "Random image";
+        container.appendChild(img);
+    });
+
